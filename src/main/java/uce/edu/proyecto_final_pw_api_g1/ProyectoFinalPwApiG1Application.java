16b8c87edccaa68374607d63e7acebb904eb4ac7
driven <aldazpaulo@gmail.com>
2024-02-22 22:47:01 -0500

    first commit

---
+++ src/main/java/uce/edu/proyecto_final_pw_api_g1/ProyectoFinalPwApiG1Application.java
@@ -0,0 +1,13 @@
+package uce.edu.proyecto_final_pw_api_g1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPwApiG1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPwApiG1Application.class, args);
+	}
+
+}
---
+++ src/test/java/uce/edu/proyecto_final_pw_api_g1/ProyectoFinalPwApiG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package uce.edu.proyecto_final_pw_api_g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalPwApiG1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
